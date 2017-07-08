2c889d9a42bbff6fd003394956ef03cdfd894851
Benjamin Strauß <benmastra@gmail.com>
2017-07-09 01:56:25 +0200

    test ios config

--- browsers.conf.js
+++ browsers.conf.js
@@ -1,99 +1,102 @@
 'use strict';
 
-const sauceLabsDesktopLaunchers = {
-  'SL_CHROME': {
-    base: 'SauceLabs',
-    browserName: 'chrome',
-    platform: 'Windows 10',
-  },
-  'SL_FIREFOX': {
-    base: 'SauceLabs',
-    browserName: 'firefox',
-    platform: 'Windows 10',
-  },
-  'SL_IE_11': {
-    base: 'SauceLabs',
-    browserName: 'internet explorer',
-    platform: 'Windows 8.1',
-    version: '11'
-  },
-  'SL_EDGE_14': {
-    base: 'SauceLabs',
-    browserName: 'MicrosoftEdge',
-    platform: 'Windows 10',
-    version: '14'
-  },
-  'SL_EDGE_15': {
-    base: 'SauceLabs',
-    browserName: 'MicrosoftEdge',
-    platform: 'Windows 10',
-    version: '15'
-  },
-  'SL_SAFARI_9': {
-    base: 'SauceLabs',
-    browserName: 'safari',
-    platform: 'OS X 10.11',
-    version: '9'
-  },
-  'SL_SAFARI_10': {
-    base: 'SauceLabs',
-    browserName: 'safari',
-    platform: 'macOS 10.12',
-    version: '10'
-  }
+const desktop = {
+  // 'SL_CHROME': {
+  //   base: 'SauceLabs',
+  //   browserName: 'chrome',
+  //   platform: 'Windows 10',
+  // },
+  // 'SL_FIREFOX': {
+  //   base: 'SauceLabs',
+  //   browserName: 'firefox',
+  //   platform: 'Windows 10',
+  // },
+  // 'SL_IE_11': {
+  //   base: 'SauceLabs',
+  //   browserName: 'internet explorer',
+  //   platform: 'Windows 8.1',
+  //   version: '11'
+  // },
+  // 'SL_EDGE_14': {
+  //   base: 'SauceLabs',
+  //   browserName: 'MicrosoftEdge',
+  //   platform: 'Windows 10',
+  //   version: '14'
+  // },
+  // 'SL_EDGE_15': {
+  //   base: 'SauceLabs',
+  //   browserName: 'MicrosoftEdge',
+  //   platform: 'Windows 10',
+  //   version: '15'
+  // },
+  // 'SL_SAFARI_9': {
+  //   base: 'SauceLabs',
+  //   browserName: 'safari',
+  //   platform: 'OS X 10.11',
+  //   version: '9'
+  // },
+  // 'SL_SAFARI_10': {
+  //   base: 'SauceLabs',
+  //   browserName: 'safari',
+  //   platform: 'macOS 10.12',
+  //   version: '10'
+  // }
 };
 
-const sauceLabsMobileLaunchers = {
+const mobile = {
   'SL_IOS_9': {
     base: 'SauceLabs',
-    browserName: 'iphone',
-    platform: 'OS X 10.10',
-    version: '9'
+    browserName: 'Safari',
+    appiumVersion: '1.6.4',
+    deviceName: 'iPhone 6 Plus Simulator',
+    deviceOrientation: 'portrait',
+    platformVersion: '9',
+    platformName: 'iOS'
   },
-  'SL_IOS_10': {
-    base: 'SauceLabs',
-    browserName: 'iphone',
-    platform: 'OS X 10.10',
-    version: '10'
-  },
-  'SL_ANDROID_4.4': {
-    base: 'SauceLabs',
-    browserName: 'android',
-    platform: 'Linux',
-    version: '4.4'
-  },
-  'SL_ANDROID_5.0': {
-    base: 'SauceLabs',
-    browserName: 'android',
-    platform: 'Linux',
-    version: '5.0'
-  },
-  'SL_ANDROID_5.1': {
-    base: 'SauceLabs',
-    browserName: 'android',
-    platform: 'Linux',
-    version: '5.1'
-  },
-  'SL_ANDROID_6.0': {
-    base: 'SauceLabs',
-    browserName: 'android',
-    platform: 'Linux',
-    version: '6.0'
-  },
-  'SL_ANDROID_7.0': {
-    base: 'SauceLabs',
-    browserName: 'android',
-    platform: 'Linux',
-    version: '7.0'
-  },
-  'SL_ANDROID_7.1': {
-    base: 'SauceLabs',
-    browserName: 'android',
-    platform: 'Linux',
-    version: '7.1'
-  }
+  // 'SL_IOS_10': {
+  //   base: 'SauceLabs',
+  //   browserName: 'iphone',
+  //   platform: 'OS X 10.10',
+  //   version: '10'
+  // },
+  // 'SL_ANDROID_4.4': {
+  //   base: 'SauceLabs',
+  //   browserName: 'android',
+  //   platform: 'Linux',
+  //   version: '4.4'
+  // },
+  // 'SL_ANDROID_5.0': {
+  //   base: 'SauceLabs',
+  //   browserName: 'android',
+  //   platform: 'Linux',
+  //   version: '5.0'
+  // },
+  // 'SL_ANDROID_5.1': {
+  //   base: 'SauceLabs',
+  //   browserName: 'android',
+  //   platform: 'Linux',
+  //   version: '5.1'
+  // },
+  // 'SL_ANDROID_6.0': {
+  //   base: 'SauceLabs',
+  //   browserName: 'android',
+  //   platform: 'Linux',
+  //   version: '6.0'
+  // },
+  // 'SL_ANDROID_7.0': {
+  //   base: 'SauceLabs',
+  //   browserName: 'android',
+  //   platform: 'Linux',
+  //   version: '7.0'
+  // },
+  // 'SL_ANDROID_7.1': {
+  //   base: 'SauceLabs',
+  //   browserName: 'android',
+  //   platform: 'Linux',
+  //   version: '7.1'
+  // }
 };
 
 module.exports = {
-  customLaunchers: Object.assign({}, sauceLabsDesktopLaunchers, sauceLabsMobileLaunchers)
+  customLaunchers: Object.assign({}, desktop, mobile)
 };
