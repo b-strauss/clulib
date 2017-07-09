8fc8eccbfee702467ed2c2bf8426b87018ab7c10
Benjamin Strauß <benmastra@gmail.com>
2017-07-10 01:16:34 +0200

    deprecate: clulib.async.forEach and clulib.async.forEachRight in favor of clulib.array.asyncForEach and clulib.array.asyncForEachRight

--- test/array/array_spec.js
+++ test/array/array_spec.js
@@ -2,6 +2,8 @@ goog.provide('clulib.array.test');
 
 goog.require('clulib.array');
 
+goog.require('goog.array');
+
 clulib.array.test.main = () => {
   describe('clulib.array', () => {
     describe('removeHoles', () => {
@@ -10,6 +12,59 @@ clulib.array.test.main = () => {
         const b = clulib.array.removeHoles([0,,,1]);
 
         expect(a).toEqual(b);
+      });
+    });
+
+    describe('asyncForEach', () => {
+      it('should execute async functions for every element in an array consecutively', done => {
+        const strings = ['first', 'second', 'third'];
+        const stringsCopy = goog.array.clone(strings);
+        const delays = [500, 200, 50];
+        let result = '';
+
+        clulib.array.asyncForEach(strings, (element, index, array) => {
+          expect(array).toEqual(strings);
+          expect(element).toEqual(array[index]);
+
+          return new Promise(resolve => {
+            setTimeout(() => {
+              result += element;
+              resolve();
+            }, delays[index]);
+          });
+        }).then(() => {
+          expect(result).toEqual(strings.join(''));
+          expect(strings).toEqual(stringsCopy);
+          done();
+        });
+      });
+    });
+
+    describe('asyncForEachRight', () => {
+      it('should execute async functions for every element in an array consecutively, in reverse order', done => {
+        const strings = ['first', 'second', 'third'];
+        const stringsCopy = goog.array.clone(strings);
+        const stringsCopyReversed = goog.array.clone(strings);
+        stringsCopyReversed.reverse();
+
+        const delays = [500, 200, 50];
+        let result = '';
+
+        clulib.array.asyncForEachRight(strings, (element, index, array) => {
+          expect(array).toEqual(strings);
+          expect(element).toEqual(array[index]);
+
+          return new Promise(resolve => {
+            setTimeout(() => {
+              result += element;
+              resolve();
+            }, delays[index]);
+          });
+        }).then(() => {
+          expect(result).toEqual(stringsCopyReversed.join(''));
+          expect(strings).toEqual(stringsCopy);
+          done();
+        });
       });
     });
   });
--- test_main.js
+++ test_main.js
@@ -1,7 +1,5 @@
 goog.provide('test_main');
 
 goog.require('clulib.array.test');
-goog.require('clulib.async.test');
 
 clulib.array.test.main();
-clulib.async.test.main();
