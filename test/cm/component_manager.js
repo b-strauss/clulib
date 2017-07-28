dec3cba5ef7455a41c05c7b8ab3ce6f7fa5f6e83
Benjamin Strauß <b.strauss@netzbewegung.com>
2017-07-28 15:57:07 +0200

    add basic tests for clulib.cm.ComponentManager

---
+++ test/cm/component_manager.js
@@ -0,0 +1,125 @@
+goog.provide('clulib.cm.ComponentManager.test');
+
+goog.require('clulib.cm.ComponentManager');
+goog.require('clulib.cm.Component');
+
+goog.require('goog.dom');
+goog.require('goog.structs.Map');
+
+clulib.cm.ComponentManager.test.main = () => {
+  describe('clulib.cm.ComponentManager', () => {
+    it('should use specific attributes for decoration', () => {
+      const manager = new clulib.cm.ComponentManager();
+
+      expect(manager.getTypeAttribute()).toBe('data-cmp');
+      expect(manager.getIdAttribute()).toBe('data-cmp-id');
+      expect(manager.getConfigAttribute()).toBe('data-cmp-cfg');
+    });
+
+    it('should accept a component for registration', () => {
+      const manager = new clulib.cm.ComponentManager();
+      const component = clulib.cm.ComponentManager.test.createDummyComponent();
+
+      manager.addComponent('foo', component);
+
+      expect(manager.getRegistry().get('foo')).toBe(component);
+    });
+
+    it('should accept multiple components for registration', () => {
+      const manager = new clulib.cm.ComponentManager();
+      const component1 = clulib.cm.ComponentManager.test.createDummyComponent();
+      const component2 = clulib.cm.ComponentManager.test.createDummyComponent();
+      const component3 = clulib.cm.ComponentManager.test.createDummyComponent();
+      const component4 = clulib.cm.ComponentManager.test.createDummyComponent();
+
+      manager.addComponentMap({
+        'one': component1,
+        'two': component2
+      });
+
+      manager.addComponentMap(new goog.structs.Map({
+        'three': component3,
+        'four': component4
+      }));
+
+      expect(manager.getRegistry().get('one')).toBe(component1);
+      expect(manager.getRegistry().get('two')).toBe(component2);
+      expect(manager.getRegistry().get('three')).toBe(component3);
+      expect(manager.getRegistry().get('four')).toBe(component4);
+    });
+  });
+};
+
+/**
+ * @param {Function=} constructorFn
+ * @param {Function=} onInitFn
+ * @param {Function=} onDisposeFn
+ * @param {(function():Promise|null)=} waitForFn
+ * @returns {function(new:clulib.cm.Component)}
+ */
+clulib.cm.ComponentManager.test.createDummyComponent = (constructorFn = null, onInitFn = null, onDisposeFn = null,
+  waitForFn = null) => {
+  waitForFn = waitForFn || (() => Promise.resolve());
+
+  /**
+   * @constructor
+   * @extends {clulib.cm.Component}
+   */
+  const Component = function () {
+    Component.base(this, 'constructor');
+
+    if (constructorFn != null)
+      constructorFn();
+  };
+
+  goog.inherits(Component, clulib.cm.Component);
+
+  /**
+   * @returns {Promise}
+   */
+  Component.prototype.waitFor = function () {
+    return Promise.all([
+      Component.base(this, 'waitFor'),
+      waitForFn()
+    ]);
+  };
+
+  Component.prototype.onInit = function () {
+    Component.base(this, 'onInit');
+    if (onInitFn != null)
+      onInitFn();
+  };
+
+  Component.prototype.onDispose = function () {
+    Component.base(this, 'onDispose');
+    if (onDisposeFn != null)
+      onDisposeFn();
+  };
+
+  return Component;
+};
+
+/**
+ * @returns {Element}
+ */
+clulib.cm.ComponentManager.test.addDummyHtml = () => {
+  const container = document.createElement('div');
+  container.innerHTML = `
+    <div class="outer" data-cmp="outer">
+      <div class="middle">
+        <div class="inner" data-cmp="inner"></div>
+      </div>
+    <div>
+  `;
+
+  goog.dom.appendChild(document.body, container);
+
+  return container;
+};
+
+/**
+ * @param {Element} element
+ */
+clulib.cm.ComponentManager.test.removeDummyHtml = element => {
+  goog.dom.removeNode(element);
+};
--- test_main.js
+++ test_main.js
@@ -2,8 +2,10 @@ goog.provide('test_main');
 
 goog.require('clulib.array.test');
 goog.require('clulib.async.Completer.test');
+goog.require('clulib.cm.ComponentManager.test');
 goog.require('clulib.dom.test');
 
 clulib.array.test.main();
 clulib.async.Completer.test.main();
+clulib.cm.ComponentManager.test.main();
 clulib.dom.test.main();
